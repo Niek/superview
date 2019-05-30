c6ddca5adfaf7f9baa07687a1a872960584d311d
Niek van der Maas <mail@niekvandermaas.nl>
2019-05-30 08:24:29 +0200

    Updated build script and gitignore

--- build.sh
+++ build.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+if [ $# -ne 1 ]; then
+  echo "Usage: ./build.sh <version number>"
+  exit
+fi
+
+VERSION=$1
+
+echo "Build packages with version number ${VERSION}"
+
 platforms=("windows/amd64" "windows/386" "darwin/amd64" "linux/386" "linux/amd64")
 
 for platform in "${platforms[@]}"
@@ -7,7 +16,7 @@ do
     platform_split=(${platform//\// })
     GOOS=${platform_split[0]}
     GOARCH=${platform_split[1]}
-    output_name='build/superview-'$GOOS'-'$GOARCH
+    output_name='build/superview-'$GOOS'-'$GOARCH'-'$VERSION
     if [ $GOOS = "windows" ]; then
         output_name+='.exe'
     fi  
