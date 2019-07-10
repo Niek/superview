dffaf180fdc5bab34b5d6e4627ad2ddab2bd33f3
Niek van der Maas <mail@niekvandermaas.nl>
2019-07-10 14:35:04 +0200

    Update build script, push release to GitHub

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 if [ $# -ne 1 ]; then
   echo "Usage: ./build.sh <version number>"
+  echo "Suggested version: "$(git describe --tags | tr -d v | awk '{printf "%.1f", $1 + .1}')
   exit
 fi
 
@@ -10,6 +11,7 @@ VERSION=$1
 echo "Build packages with version number ${VERSION}"
 
 platforms=("windows/amd64" "windows/386" "darwin/amd64" "linux/386" "linux/amd64")
+files=()
 
 for platform in "${platforms[@]}"
 do
@@ -26,7 +28,10 @@ do
         echo 'An error has occurred! Aborting the script execution...'
         exit 1
     fi
+
+    files+=($output_name)
 done
 
 git tag v${VERSION}
 git push origin --tags
+hub release create -do $(for f in "${files[@]}"; do echo "-a "$f; done) -m "Release v${VERSION}" v${VERSION}
