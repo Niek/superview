c1f1b670a12e27f4bcc954c3ba8aa543d6bb842e
Niek van der Maas <mail@niekvandermaas.nl>
2020-07-10 17:44:14 +0200

    Support for building GUI

--- build.sh
+++ build.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
 if [ $# -ne 1 ]; then
-  echo "Usage: ./build.sh <version number>"
-  echo "Suggested version: "$(git describe --tags | tr -d v | awk '{printf "%.1f", $1 + .1}')
-  exit
+    echo "Usage: ./build.sh <version number>"
+    echo "Suggested version: "$(git describe --tags | tr -d v | awk '{printf "%.1f", $1 + .1}')
+    exit
+fi
+
+if ! command -v fyne-cross &> /dev/null; then
+    echo "This build script requires fyne-cross to be installed:"
+    echo "go get github.com/lucor/fyne-cross/cmd/fyne-cross"
+    exit
 fi
 
 VERSION=$1
@@ -13,25 +19,43 @@ echo "Build packages with version number ${VERSION}"
 platforms=("windows/amd64" "windows/386" "darwin/amd64" "linux/386" "linux/amd64")
 files=()
 
-for platform in "${platforms[@]}"
-do
-    platform_split=(${platform//\// })
-    GOOS=${platform_split[0]}
-    GOARCH=${platform_split[1]}
-    output_name='build/superview-'$GOOS'-'$GOARCH'-v'$VERSION
-    if [ $GOOS = "windows" ]; then
-        output_name+='.exe'
-    fi  
+for program in "superview" "superview-gui"; do
+    for platform in "${platforms[@]}"; do
+        platform_split=(${platform//\// })
+        GOOS=${platform_split[0]}
+        GOARCH=${platform_split[1]}
 
-    env GOOS=$GOOS GOARCH=$GOARCH go build -o $output_name .
-    if [ $? -ne 0 ]; then
-        echo 'An error has occurred! Aborting the script execution...'
-        exit 1
-    fi
+        if [ "$program" == "superview" ]; then
+            output_name="build/${program}-${GOOS}-${GOARCH}-v${VERSION}"
+            if [ $GOOS == "windows" ]; then
+                output_name+=".exe"
+            fi
+            
+            env GOOS=$GOOS GOARCH=$GOARCH go build -o $output_name ${program}.go common.go
+            if [ $? -ne 0 ]; then
+                echo "An error has occurred! Aborting the script execution..."
+                exit 1
+            fi
+        else
+            output_name="${program}-${GOOS}-${GOARCH}-v${VERSION}"
+            fyne-cross ${GOOS} -silent -arch ${GOARCH} -output ${output_name} "${program}.go common.go"
+            output_name="fyne-cross/dist/${GOOS}-${GOARCH}/${output_name}"
+            if [ $GOOS == "windows" ]; then
+                output_name+=".zip"
+            elif [ $GOOS == "linux" ]; then
+                output_name+=".tar.gz"
+            else
+                output_name+=".app"
+            fi
+        fi
 
-    files+=($output_name)
+        echo "Built: ${output_name}"
+
+        files+=($output_name)
+    done
 done
 
+exit
 git tag v${VERSION}
 git push origin --tags
 hub release create -do $(for f in "${files[@]}"; do echo "-a "$f; done) -m "Release v${VERSION}" v${VERSION}
